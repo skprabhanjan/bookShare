fc1bd3b4ae6218497a918fec578391f56412a9dd
Prabhanjan SK <prabhanjansk@Prabhanjans-MBP.Home>
2017-04-04 20:40:45 +0530

    Displaying books in dashboard

--- app/js/core/services/authService.js
+++ app/js/core/services/authService.js
@@ -1,6 +1,6 @@
 app.factory('authUser', ['$http','$state','$q', function($http,$state, $q) {
 
-	var host = "https://2b67c30a.ngrok.io"; // backend server
+	var host = "https://af2a2beb.ngrok.io"; // backend server
 	var token = ''; // token to send for Authorization of api calls
 
 	//get the token required to make all the api calls
