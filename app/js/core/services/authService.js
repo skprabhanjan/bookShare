de1821bf7f8556c0b062af2c3378640fa1b2709e
SudhanvaMG <mgsudhanva@gmail.com>
2017-04-06 18:48:51 +0530

    my adds

--- app/js/core/services/authService.js
+++ app/js/core/services/authService.js
@@ -1,7 +1,7 @@
 app.factory('authUser', ['$http','$state','$q', function($http,$state, $q) {
 
-	var host = "https://ad91999c.ngrok.io"; // backend server praj
-	// var host = "https://6c9e09f1.ngrok.io"; // backend server td
+	var host = "https://4f924576.ngrok.io"; // backend server praj
+	//var host = "https://6c9e09f1.ngrok.io"; // backend server td
 
 	var token = ''; // token to send for Authorization of api calls
 
@@ -122,6 +122,34 @@ app.factory('authUser', ['$http','$state','$q', function($http,$state, $q) {
 					var req = {
 						 method: 'GET',
 						 url: host + '/books/getAll',
+						 headers: {
+						   'Authorization': 'Bearer ' + token
+						 }
+					 }
+					$http(req).success(
+						function(resp){
+								def.resolve(resp);
+								//user found
+						})
+						.error(
+							function(){
+								def.reject("error");
+							console.log("error");
+						});
+
+				});
+			//create an user acccount
+		return def.promise;
+		},
+		getsoldbooks:function(email){
+				var def = $q.defer();
+			$http.post(host + '/authenticate',{userName:window.btoa("bookShare"),password:window.btoa("nodejs")}).success(
+				function(resp){
+					token  = resp.token;
+
+					var req = {
+						 method: 'GET',
+						 url: host + '/users/sellbox?email='+email,
 						 headers: {
 						   'Authorization': 'Bearer ' + token
 						 }
